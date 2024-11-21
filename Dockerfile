5e5cee903240a84eee99e9a4bd98b44ef2ad4952
suyashagrahari <suyashagrahari73@gmail.com>
2024-11-21 17:11:15 +0530

    adding docker file and docker-compose file

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Node.js runtime as the base image
+FROM node:16
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code
+COPY . .
+
+# Expose the application's port
+EXPOSE 4000
+
+# Start the application
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -9,16 +9,44 @@ dotenv.config();
 // Create Express app
 const app = express();
 
-// Middleware
+// Middleware to log IP and Port
+app.use((req, res, next) => {
+  const ip = req.ip; // Get the IP address of the client
+  const port = req.connection.localPort; // Get the port of the server (request's local port)
+
+  console.log(
+    `Request received from IP: ${ip}, Port: ${port}, Path: ${req.originalUrl}`
+  );
+  next(); // Pass control to the next middleware
+});
+
+// Middleware for parsing JSON requests
 app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
 
+// Routes
 app.get("/", async (req, res) => {
   res.status(200).json("hello suyash");
 });
-// Routes
+
+app.get("/app1", async (req, res) => {
+  res.status(200).json("Hello I am App1");
+});
+
+app.get("/app2", async (req, res) => {
+  res.status(200).json("Hello I am App2");
+});
+
+app.get("/app3", async (req, res) => {
+  res.status(200).json("Hello I am App3");
+});
+app.get("/suyash", async (req, res) => {
+  res.status(200).json("Docker Update testing1");
+});
+
+// User routes
 app.use("/api/users", userRoutes);
 
 // Start server
